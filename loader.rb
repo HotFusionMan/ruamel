12cfb4c5e51ddcebbba28595ecf2fc21aa988ed1
Al Chou <HotFusionMan+GitHub@Gmail.com>
2022-08-08 22:10:03 -0700

    Ported loader.py.

---
+++ loader.rb
@@ -0,0 +1,65 @@
+# encoding: utf-8
+
+# frozen_string_literal: true
+
+require 'reader'
+require 'scanner'
+require 'parser'
+require 'composer'
+require 'constructor'
+require 'resolver'
+
+module SweetStreetYaml
+  class BaseLoader
+    extend Forwardable
+
+    attr_accessor :_reader,
+                  :_scanner,
+                  :_parser,
+                  :_composer,
+                  :_constructor,
+                  :_resolver,
+                  :comment_handling,
+                  :version, :tags, :parsed_comments
+
+    def_delegators :@_composer, :check_node, :get_single_node
+    def_delegator :@_parser, :dispose
+    def_delegator :@_resolver, :processing_version
+
+    def initialize(stream, version = nil, preserve_quotes = nil)
+      @comment_handling = nil
+      @_reader = Reader.new(stream, self)
+      @_scanner = Scanner.new(self)
+      @_parser = Parser.new(self)
+      @_composer = Composer.new(self)
+      @_constructor = BaseConstructor.new(:loader => self)
+      @_resolver = VersionedResolver.new(:version => version, :loadumper => self)
+    end
+  end
+
+
+  class Loader < BaseLoader
+    def initialize(stream, version = nil, preserve_quotes = nil)
+      super
+      @_constructor = BaseConstructor.new(:loader => self)
+    end
+  end
+
+
+  class SafeLoader < Loader
+    def initialize(stream, version = nil, preserve_quotes = nil)
+      super
+      @_constructor = SafeConstructor.new(:loader => self)
+    end
+  end
+
+
+  class RoundTripLoader < Loader
+    def initialize(stream, version = nil, preserve_quotes = nil)
+      super
+      @_scanner = RoundTripScanner.new(self)
+      @_parser = RoundTripParser.new(self)
+      @_constructor = RoundTripConstructor.new(:preserve_quotes => preserve_quotes, :loader => self)
+    end
+  end
+end
